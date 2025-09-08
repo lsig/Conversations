1e701024e3cfc0f80e105bad525d4f854e45f84f
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-09-08 15:25:33 -0400

    add changes

--- players/player_11/player.py
+++ players/player_11/player.py
@@ -1,4 +1,5 @@
 from models.player import Item, Player, PlayerSnapshot
+from players.player_11.do_something import do_something
 
 
 class Player11(Player):
@@ -6,4 +7,7 @@ class Player11(Player):
 		super().__init__(snapshot, conversation_length)
 
 	def propose_item(self, history: list[Item]) -> Item | None:
-		return None
+        do_something()
+        for mb in self.memory_bank:
+            print(f"importance: {mb.importance}")
+        return None
