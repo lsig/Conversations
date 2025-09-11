8df48f48d200f76dd3e95c1625308247bb2b2897
Bryce Washington <brlw223@gmail.com>
2025-09-10 21:22:21 -0400

    test push

--- players/player_9/player.py
+++ players/player_9/player.py
@@ -1,6 +1,6 @@
 from models.player import Item, Player, PlayerSnapshot
 
-
+#test
 class Player9(Player):
 	def __init__(self, snapshot: PlayerSnapshot, conversation_length: int) -> None:  # noqa: F821
 		super().__init__(snapshot, conversation_length)
