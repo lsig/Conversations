bed679e908387fa022ef936a567f81722ab08a2e
jparkhouse2024 <jparkhouse20@gmail.com>
2025-09-15 10:06:01 -0400

    Delete players directory

--- players/pause_player.py
+++
@@ -1,9 +0,0 @@
-from models.player import GameContext, Item, Player, PlayerSnapshot
-
-
-class PausePlayer(Player):
-	def __init__(self, snapshot: PlayerSnapshot, ctx: GameContext) -> None:  # noqa: F821
-		super().__init__(snapshot, ctx)
-
-	def propose_item(self, history: list[Item]) -> Item | None:
-		return None
