589b060d7f2b8e45804cbec226f3ab3dad85c406
kna2121 <kna2121@columbia.edu>
2025-09-14 21:55:00 -0400

    ruff formatting

--- players/player_7/player.py
+++ players/player_7/player.py
@@ -1,4 +1,3 @@
-
 from models.player import GameContext, Item, Player, PlayerSnapshot
 
 
@@ -15,13 +14,10 @@ class Player7(Player):
 			return self.pause(history)
 		else:
 			return self.play(history)
-		
-
 
 	def pause(self, history: list[Item]) -> Item | None:
-		
 		rejected: list[Item] = list()
-		
+
 		# look through preferences in most to least important order
 		for p in self.preferences:
 			# when history is less than 5, just propose the first item that matches preference and has high importance
@@ -29,18 +25,18 @@ class Player7(Player):
 				for item in self.memory_bank:
 					if p in item.subjects and item.importance > 0.5:
 						return item
-					
+
 			# check history of last 5 items to see if preference has been mentioned recently and if it has skip
 			elif len(history) >= 5 and p not in history[:-5]:
 				for item in self.memory_bank:
-					#check if p is in the subejcts of an item, not in history, and greater importance than arbitrary theshold
+					# check if p is in the subejcts of an item, not in history, and greater importance than arbitrary theshold
 					if p in item.subjects and item not in history and item.importance > 0.5:
 						return item
 					else:
 						rejected.append(item)
-		
+
 		return rejected[0] if rejected else None
-	
+
 	def play(self, history: list[Item]) -> Item | None:
 		subject_count = {subject: 0 for subject in self.preferences}
 		# tracks how many times each subject has been mentioned in the last 3 said items
@@ -61,13 +57,20 @@ class Player7(Player):
 				continue
 			for subject in item.subjects:
 				times_mentioned = subject_count[subject]
-				pref_index = self.preferences.index(subject) # get index of subject in preferences list
-				if times_mentioned in range(1, 3) and subject in self.preferences[0:preference_threshold]:  # noqa: SIM102
-					if pref_index < preference_threshold or (pref_index ==highest_pref_index and item.importance > importance):
+				pref_index = self.preferences.index(
+					subject
+				)  # get index of subject in preferences list
+				if (
+					times_mentioned in range(1, 3)
+					and subject in self.preferences[0:preference_threshold]
+				):  # noqa: SIM102
+					if pref_index < preference_threshold or (
+						pref_index == highest_pref_index and item.importance > importance
+					):
 						chosen_item = item
 						importance = item.importance
 						highest_pref_index = pref_index
-		
+
 		# If no item has been chosen so far, then loop through memory bank and find the item that has highest importance and is not in history.
 		if chosen_item is None:
 			for item in self.memory_bank:
@@ -76,7 +79,3 @@ class Player7(Player):
 					importance = item.importance
 		# Return item with highest importance that is not in history.
 		return chosen_item if chosen_item else None
-
-			
-
-		
