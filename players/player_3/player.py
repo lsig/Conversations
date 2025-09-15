3a8894a38d3461c0f5d67ed9a677a82f4b936b11
jparkhouse2024 <jparkhouse20@gmail.com>
2025-09-15 10:39:09 -0400

    Update player.py

--- players/player_3/player.py
+++ players/player_3/player.py
@@ -1,29 +1,27 @@
-from models.player import Item, Player, PlayerSnapshot
+import heapq
+
+from models.player import GameContext, Item, Player, PlayerSnapshot
 
 
 class Player3(Player):
-<<<<<<< HEAD
-	def __init__(self, snapshot: PlayerSnapshot, conversation_length: int) -> None:  # noqa: F821
-		super().__init__(snapshot, conversation_length)
-=======
-	def __init__(self, snapshot: PlayerSnapshot, ctx: GameContext) -> None:  # noqa: F821
-		super().__init__(snapshot, ctx)
-		self.ID_dict = dict()
-		# maps IDs to items
+	def __init__(self, snapshot: PlayerSnapshot, ctx: GameContext) -> None:  # noqa: F821 
+		super().__init__(snapshot, ctx) 
+		self.ID_dict = dict() 
+		# maps IDs to items 
 
-		self.blocks = dict()
-		# Good conversation continuers "middle of the zipper"
+		self.blocks = dict() 
+		# Good conversation continuers "middle of the zipper" 
 
-		self.High_topics = self.preferences[: len(self.preferences) // 2]
+		self.High_topics = self.preferences[: len(self.preferences) // 2] 
 
-		self.started_subject = set()
+		self.started_subject = set() 
 
-		self.previous_item = [-1]
+		self.previous_item = [-1] 
 
 		# Sets self.starters to be a heap of items that have high importance and high self value
 		# sets self.blocks to be a heap of items that have high importance
-		# optimal_p = Player3.best_p_value(self)
-		print('optimal_p', optimal_p)
+		optimal_p = Player3.best_p_value(self)
+		#print('optimal_p', optimal_p)
 		for item in self.memory_bank:
 			self.ID_dict[item.id] = item
 			if len(item.subjects) == 2:
@@ -61,7 +59,77 @@ class Player3(Player):
 			else:
 				self.blocks[item.subjects[0]] = []
 				heapq.heappush(self.blocks[item.subjects[0]], (-item.importance, item.id))
->>>>>>> parent of f20ced5 (Actually fixed the print issue)
 
 	def propose_item(self, history: list[Item]) -> Item | None:
-		return None
+		# Maintain a set of topics already started
+		if len(history) == 1 and history[-1] is not None:
+			self.started_subject.add(history[-1].subjects[0])
+		if len(history) == 2 and history[-1] is not None:
+			self.started_subject.add(history[-1].subjects[0])
+		elif len(history) > 2:
+			if history[-2] is None and history[-1] is not None:
+				self.started_subject.add(history[-1].subjects[0])
+			if history[-3] is None and history[-1] is not None:
+				self.started_subject.add(history[-1].subjects[0])
+
+		if (
+			len(history) > 0
+			and self.previous_item[0] != -1
+			and history[-1] != self.ID_dict[self.previous_item[0]]
+		):
+			Player3.readd(self, self.ID_dict[self.previous_item[0]])
+
+		# Start a conversation with our best conversation opener
+		if len(history) < 2 or history[-1] is None:
+			best_subject = None
+			best_importance = 0
+			for subject in self.blocks:
+				if subject in self.started_subject:
+					continue
+				if len(self.blocks[subject]) > 0:
+					(cur_importance, id) = self.blocks[subject][0]
+					if -cur_importance > best_importance:
+						best_subject = subject
+						best_importance = -cur_importance
+			if best_subject is None:
+				self.previous_item = [-1]
+				return None
+			else:
+				(score, item_id) = heapq.heappop(self.blocks[best_subject])
+				self.previous_item[0] = item_id
+				return self.ID_dict[item_id]
+
+		# Start a second conversation with our best conversation opener
+		# While making sure we don't repeat a topic
+		elif history[-2] is None:
+			avoid = history[-1].subjects[0]
+			best_subject = None
+			best_importance = 0
+			for subject in self.blocks:
+				if subject in self.started_subject:
+					continue
+				if subject == avoid:
+					continue
+				if len(self.blocks[subject]) > 0:
+					(cur_importance, id) = self.blocks[subject][0]
+					if -cur_importance > best_importance:
+						best_subject = subject
+						best_importance = -cur_importance
+			if best_subject is None:
+				self.previous_item = [-1]
+				return None
+			else:
+				(score, item_id) = heapq.heappop(self.blocks[best_subject])
+				self.previous_item[0] = item_id
+				return self.ID_dict[item_id]
+
+		# Continue the conversation
+		else:
+			goalsuit = history[-2].subjects[0]
+			if goalsuit not in self.blocks or len(self.blocks[goalsuit]) == 0:
+				self.previous_item = [-1]
+				return None
+			else:
+				(score, item_id) = heapq.heappop(self.blocks[goalsuit])
+				self.previous_item[0] = item_id
+				return self.ID_dict[item_id]
