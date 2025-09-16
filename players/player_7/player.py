b570909765ad5dfbdf020ee5b1bdb1ddf2cf03d9
kna2121 <kna2121@columbia.edu>
2025-09-16 19:19:42 -0400

    changed calculation

--- players/player_7/player.py
+++ players/player_7/player.py
@@ -92,7 +92,8 @@ class Player7(Player):
 	def dynamic_threshold(self,history:list[Item])->int:
 		# return a dynamic threshold based on the history length
 		remaining = [item for item in self.memory_bank if item not in history]
-		progress = 1.0 - len(remaining) / max(1, len(self.memory_bank))  # 0→1
+		said = len(self.contributed_items)
+		progress = min(1, said/ (max(1,len(self.memory_bank)) ))  # 0→1
 		S = len(self.preferences)
 		K = int(S * (0.50 + 0.40 * progress)) - 1  # 50% -> 90% as bank shrinks
 		return max(0, min(S - 1, K))
